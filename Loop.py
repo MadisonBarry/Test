41feb197ba8e8079ee1472abbf2b581422a9acc5
s288372 <s288372@shhac150M07.ocsb.local>
2022-05-09 12:19:25 -0400

    Commit

---
+++ Loop.py
@@ -0,0 +1,35 @@
+"""
+from pip._vendor.html5lib._ihatexml import letter
+for counter in range(1, 3, 5):
+    print(counter)
+    print("Hello")
+    
+    
+
+myName = "Madison.Barry"
+
+for letter in myName:
+    print(letter)
+
+"""
+
+'''
+number = 1
+
+while number <10:
+    print("Hello")
+    number = number +1
+    
+'''
+
+
+"""
+moo = input("Please enter a number: ")
+"""
+count = 0
+userInput = input("please enter a number: ")
+
+while count < userInput:
+    print("Hello world!")
+    # count +=1
+    count = count + 1
