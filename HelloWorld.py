d60f1cf3c3b1d2d6451f740fe2829cdaaf328185
s288372 <s288372@shhac150M07.ocsb.local>
2022-05-09 12:13:18 -0400

    commit

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+print("Please enter a number higher than 5:")
+
+
+user_number = int(input())
+
+if user_number < 5:
+    print("You didn't follow the instructions")
+    
+    
+if user_number > 10:
+    print("Good work")
+    
+
+if user_number < 5:
+    print("Really? Is that you can do")
+    
+    
+print ("Have a nice day")    
