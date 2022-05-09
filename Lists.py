c44c93c9df42f577bfbe322ee9cfd831182027f5
s288372 <s288372@shhac150M07.ocsb.local>
2022-05-09 12:17:24 -0400

    Commit

---
+++ Lists.py
@@ -0,0 +1,8 @@
+barryList = []
+
+barryList2 = [1, "foo", 3.3, 'x']
+
+print(barryList) #Print entire list
+print(barryList2) #Print entire list
+print(barryList2[2]) #Prints 3rd element in list
+print(barryList2[1:3]) #Prints a range of elements
