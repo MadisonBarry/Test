d97069d1a184f75d9a3575281e924ae2d1e4bb55
s288372 <s288372@shhac150M07.ocsb.local>
2022-05-09 12:26:52 -0400

    Commit

---
+++ Strings.py
@@ -0,0 +1,31 @@
+"""
+Strings are a datatype.
+Strings can be changed.
+Strings are written single or double quotation marks.
+All user unput are strings.
+"""
+from pickletools import string1
+
+
+string1 = "This is a string."
+#string2 = 'This is also a string.'
+
+#print(string1)
+print(string1[5])
+
+#print(string2)
+#var1 = input("Please enter some text:")
+
+#print("You entered: " + var1)
+
+print(string1[2:6])
+print(string1[1:3])
+print(string1[:6])
+print(string1[6:])
+
+
+var1 = ord('c')
+print(var1)
+
+var2 = chr(55)
+print(var2)
